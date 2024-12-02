51786dc6efd32e0c0bdf63995e36d3425b725e22
Jhoselin Quiroga <jhoselin.quiroga@ucb.edu.bo>
2024-12-02 11:25:32 -0400

    Validate the link for ax-animate.gif image

--- features/step_definitions/browsersTestPage.rb
+++ features/step_definitions/browsersTestPage.rb
@@ -3,21 +3,30 @@ Given(/^I am on the GMO Browser Test page$/) do
     visit('https://demo.borland.com/gmopost/browser-test.htm')
   end
   
-  When(/^I click on the "Green Mountain Output" image$/) do
-    find(:xpath, '//input[@type="image" and @name="I3"]').click
+When(/^I click on the "Green Mountain Output" image$/) do
+find(:xpath, '//input[@type="image" and @name="I3"]').click
+end
+
+Then(/^I should be redirected to the error page with the URL "(.*)"$/) do |expected_url|
+actual_url = page.current_url
+expect(actual_url).to eq(expected_url) 
+end
+
+When(/^I click on the "Java Applet" image$/) do
+find(:xpath, '//img[@src="images/java-animate2.gif"]').click
+end
+
+Then(/^I should be redirected to the Java Applet page with the URL "(.*)"$/) do |expected_url|
+actual_url = page.current_url
+expect(actual_url).to eq(expected_url)
+end
+
+When(/^I click on the "ActiveX" image$/) do
+    find(:xpath, '//img[@src="images/ax-animate.gif"]').click
   end
-  
-  Then(/^I should be redirected to the error page with the URL "(.*)"$/) do |expected_url|
-    actual_url = page.current_url
-    expect(actual_url).to eq(expected_url) 
-  end
-  
-#   Java Applet (java-animate2.gif)
-  When(/^I click on the "Java Applet" image$/) do
-    find(:xpath, '//img[@src="images/java-animate2.gif"]').click
-  end
-  
-  Then(/^I should be redirected to the Java Applet page with the URL "(.*)"$/) do |expected_url|
+
+Then(/^I should be redirected to the ActiveX page with the URL "(.*)"$/) do |expected_url|
     actual_url = page.current_url
     expect(actual_url).to eq(expected_url)
   end
+  
