68650eaefef17d5b03a77f23c525b5bf7e201b2f
denilsFiesta <denilson.fiesta@ucb.edu.bo>
2024-12-04 18:32:06 -0400

    Hooks agregados

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,3 +1,9 @@
+After do |scenario|
+  if scenario.failed?
+    save_screenshot("screenshots/#{scenario.name}.png")
+  end
+  Capybara.reset_sessions!
+end
 
 After do 
     Capybara.current_session.driver.quit
@@ -5,5 +11,12 @@ end
 
 Before '@maximize' do
   page.driver.browser.manage.window.maximize
+end
+
+Around do |scenario, block|
+  start_time = Time.now
+  block.call
+  duration = Time.now - start_time
+  puts "El escenario '#{scenario.name}' tardó #{duration.round(2)} segundos en ejecutarse."
 end
 
